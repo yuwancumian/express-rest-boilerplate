c36d6411281472e95e4ebab0da53a520753dea37
yuwacumian <yuwancumian@hotmail.com>
2020-02-19 13:19:31 +0800

    a basic route

--- src/config/express.js
+++ src/config/express.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('../api/routes/v1');
+
 const app = express();
+app.use(bodyParser.json());
+
+app.use('/v1', route);
 
 module.exports = app;

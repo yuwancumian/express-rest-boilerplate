7ddf5a5429fd10e36f8ae2f9a7f92cbc6337a8bd
yuwacumian <yuwancumian@hotmail.com>
2020-02-23 16:42:00 +0800

    use morgin log middle ware

--- src/config/express.js
+++ src/config/express.js
@@ -3,16 +3,13 @@ const bodyParser = require('body-parser');
 const compress = require('compression');
 const cors = require('cors');
 const helmet = require('helmet');
+const morgan = require('morgan');
 const error = require('../api/middlewares/error');
 
 const route = require('../api/routes/v1');
 
 const app = express();
-app.use(function(req, res, next) {
-  console.log('Request IP: ' + req.url);
-  console.log('Request date: ' + new Date());
-  next(); // 新的这行很重要
-});
+app.use(morgan('short'));
 
 app.use(helmet());
 app.use(compress());
